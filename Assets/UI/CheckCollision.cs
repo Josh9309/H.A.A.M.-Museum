c2b00f08e98bd2d9eeaca366ff80fb76bc7934f8
Kevin Idzik <kdi4293@rit.edu>
2016-12-04 23:51:44 -0500

    Beginning Elevator

--- Assets/UI/CheckCollision.cs
+++ Assets/UI/CheckCollision.cs
@@ -5,6 +5,7 @@ public class CheckCollision : MonoBehaviour
 {
     private Canvas interactionCanvas; //The canvas displaying interaction information
     private Canvas itemCanvas; //The canvas displaying item information
+    private UnityEngine.UI.Text interactionMessage; //The interaction message
     private UnityEngine.UI.Text itemTitle; //The item's title
     private UnityEngine.UI.Text itemDescription; //The item's description
     private MeshFilter model; //The model to render
@@ -28,6 +29,8 @@ public class CheckCollision : MonoBehaviour
                 itemCanvas = canvases[i]; //Get the item canvas
             }
         }
+
+        interactionMessage = interactionCanvas.GetComponentInChildren<UnityEngine.UI.Text>(); //Get the message text
 
         UnityEngine.UI.Text[] textElements = itemCanvas.GetComponentsInChildren<UnityEngine.UI.Text>(); //Get the UI text
 
@@ -66,41 +69,53 @@ public class CheckCollision : MonoBehaviour
 
     void OnTriggerStay(Collider coll) //Check for trigger stay
     {
-        if (!interactionCanvas.enabled) //If the interaction canvas is disabled
+        if (coll.gameObject.name.Contains("Elevator")) //If the trigger is for the elevator
         {
-            interactionCanvas.enabled = true; //Enable the interaction canvas
-
-            Trigger triggerScript = coll.GetComponent<Trigger>(); //Get the trigger script on the object
-            itemTitle.text = triggerScript.ItemTitle; //Change the title text
-            itemDescription.text = triggerScript.ItemDescription; //Change the description text
-
-            model.transform.rotation = coll.gameObject.transform.rotation; //Rotate the painting accordingly
-
-            model.mesh = coll.GetComponent<MeshFilter>().mesh; //Get the model on the object
-            modelTextureRenderer.material = coll.GetComponent<Renderer>().material; //Get the material on the object's texture renderer
-
-            rotateModelScript.ShouldRotate = triggerScript.ShouldRotate; //Set if the model should rotate
-
-            rotateModelScript.RotateAroundX = triggerScript.RotateAroundX; //Set which axis to rotate around
+            if (!interactionCanvas.enabled) //If the interaction canvas is disabled
+            {
+                interactionCanvas.enabled = true; //Enable the interaction canvas
+                interactionMessage.text = "Press 1 for floor 1\nPress 2 for floor 2\nPress 3 for floor 3"; //Set the message text
+            }
         }
-        else if (interactionCanvas.enabled && Input.GetButton("Fire1")) //If the interaction canvas is enabled and the player left clicks
+        else //If the trigger is not an elevator
         {
-            fpsController.enabled = false; //Disable the player
+            if (!interactionCanvas.enabled) //If the interaction canvas is disabled
+            {
+                interactionCanvas.enabled = true; //Enable the interaction canvas
+                interactionMessage.text = "Left click to examine"; //Set the message text
 
-            model.transform.position = modelIdentity.transform.position; //Move the model back into view
-            itemCanvas.enabled = true; //Enable the item canvas
-        }
-        else if (itemCanvas.enabled && Input.GetButton("Fire2")) //If the item canvas is enabled and the player right clicks
-        {
-            fpsController.enabled = true; //Enable the player
+                Trigger triggerScript = coll.GetComponent<Trigger>(); //Get the trigger script on the object
+                itemTitle.text = triggerScript.ItemTitle; //Change the title text
+                itemDescription.text = triggerScript.ItemDescription; //Change the description text
 
-            interactionCanvas.enabled = false; //Disable the interaction canvas
+                model.transform.rotation = coll.gameObject.transform.rotation; //Rotate the painting accordingly
 
-            rotateModelScript.ShouldRotate = false; //Stop the model from rotating
-            model.transform.rotation = modelIdentity.transform.rotation; //Reset the model
+                model.mesh = coll.GetComponent<MeshFilter>().mesh; //Get the model on the object
+                modelTextureRenderer.material = coll.GetComponent<Renderer>().material; //Get the material on the object's texture renderer
 
-            model.transform.position = new Vector3(model.transform.position.x, -100, model.transform.position.z); //Move the model real far away
-            itemCanvas.enabled = false; //Disable the item canvas
+                rotateModelScript.ShouldRotate = triggerScript.ShouldRotate; //Set if the model should rotate
+
+                rotateModelScript.RotateAroundX = triggerScript.RotateAroundX; //Set which axis to rotate around
+            }
+            else if (interactionCanvas.enabled && Input.GetButton("Fire1")) //If the interaction canvas is enabled and the player left clicks
+            {
+                fpsController.enabled = false; //Disable the player
+
+                model.transform.position = modelIdentity.transform.position; //Move the model back into view
+                itemCanvas.enabled = true; //Enable the item canvas
+            }
+            else if (itemCanvas.enabled && Input.GetButton("Fire2")) //If the item canvas is enabled and the player right clicks
+            {
+                fpsController.enabled = true; //Enable the player
+
+                interactionCanvas.enabled = false; //Disable the interaction canvas
+
+                rotateModelScript.ShouldRotate = false; //Stop the model from rotating
+                model.transform.rotation = modelIdentity.transform.rotation; //Reset the model
+
+                model.transform.position = new Vector3(model.transform.position.x, -100, model.transform.position.z); //Move the model real far away
+                itemCanvas.enabled = false; //Disable the item canvas
+            }
         }
     }
 
